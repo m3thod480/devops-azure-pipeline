16049f4e7f2337a8cb66b40f733df925bb987073
m3thod480 <jjbach99@gmail.com>
2026-05-01 20:00:58 +0200

    Se añaden los hosts al setting

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-&=fg73y6@lgld3j7t@bs!yk=naqw1+ro5nt+ts4ffk((e)0k8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["loocalhost",
-    "127.0.0.1",
-    "host.docker.internal",
-    ]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
